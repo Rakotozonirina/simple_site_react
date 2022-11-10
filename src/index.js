f4f1b36ac685135f9b24a4bbd81dc4062638ed8f
Zo Nirina <tomzonirina@gmail.com>
2022-11-10 10:13:35 +0300

    the start master page

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import  ReactDOM  from 'react'
+
+function App(){
+    return(
+        <>
+
+        </>
+    )
+}
+ReactDOM.render(App, document.getElementById("root"))
