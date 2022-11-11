4ba8475d9476c3cab260c284c11a6ca828f8644c
Zo Nirina <tomzonirina@gmail.com>
2022-11-11 22:49:16 +0300

    I include a logo

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import '../index.css'
+import image from '../images/logo192.png'
 export default function Nav(){
     return(
         <nav>
-
+            <img src={image} className="logo" />
         </nav>
     )
 }
