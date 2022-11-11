bcab7df9cd05430571eab44e93ff4467360d888e
Zo Nirina <tomzonirina@gmail.com>
2022-11-11 22:31:50 +0300

    I am include more things

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
-
+import Nav from './component/navbar'
 export default function App() {
     return(
         <>
-        <nav>
-            
-        </nav>
+            <Nav/>
         </>
     )
 }
