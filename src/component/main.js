29a26b3c64c701bfc35c3042583263f4a25f075c
Zo Nirina <tomzonirina@gmail.com>
2022-11-12 08:27:36 +0300

    I am finish the main file

---
+++ src/component/main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../index.css'
+export default function Main() {
+    return(
+        <div className='content-main'>
+            <ul>
+                <li>Was first realesed in 2013</li>
+                <li>Was Originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>is maintained by Facebook</li>
+                <li>Powers thousands of entreprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    )
+}
