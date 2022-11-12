bc70f619ae00770f0e3e5d58447011ac52308f60
Zo Nirina <tomzonirina@gmail.com>
2022-11-12 07:38:24 +0300

    I include a heading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Nav from './component/navbar'
+import Heading from './component/heading'
 export default function App() {
     return(
         <>
             <Nav/>
+            <Heading/>
         </>
     )
 }
---
+++ src/component/heading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../index.css'
+export default function Heading(){
+    return(
+        <div className='contenaire_heading'>
+            <h1 className='heading-one'>Fun facts about React</h1>
+        </div>
+    )
+}
