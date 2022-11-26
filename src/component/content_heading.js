952645df732af9eb4779d10218f53b5051be1a7c
Zo Nirina <tomzonirina@gmail.com>
2022-11-26 12:34:55 +0300

    I done the one step of the card

--- src/component/content_heading.js
+++ src/component/content_heading.js
@@ -3,6 +3,8 @@ import React from 'react'
 export default function Heading(){
     return(
         <div className='contenus'>
+            <h2 className='font-effect-fire-animation'>Zo Nirina</h2>
+            <p>Web designer and front-end developer</p>
         </div>
     )
 }
--- src/component/main.js
+++ src/component/main.js
@@ -1,11 +1,13 @@
 import React from "react"
 import '../index.css'
 import ContentPhoto from "./content_photo"
+import Heading from "./content_heading"
 export default function Main() {
     return(
         <div>
             <main>
                 <ContentPhoto></ContentPhoto>
+                <Heading></Heading>
             </main>
         </div>
     )
