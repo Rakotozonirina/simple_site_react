118e484decd8c4ba64683d4c50f5d66bdb7dbba9
Zo Nirina <tomzonirina@gmail.com>
2022-11-26 14:22:36 +0300

    the button is finish

--- src/component/content_heading.js
+++ src/component/content_heading.js
@@ -4,7 +4,7 @@ export default function Heading(){
     return(
         <div className='contenus'>
             <h2 className='font-effect-fire-animation'>Zo Nirina</h2>
-            <p>Web designer and front-end developer</p>
+            <p className='font-effect-shadow-multiple'>Web designer and front-end developer</p>
         </div>
     )
 }
--- src/component/main.js
+++ src/component/main.js
@@ -2,12 +2,14 @@ import React from "react"
 import '../index.css'
 import ContentPhoto from "./content_photo"
 import Heading from "./content_heading"
+import Button from "./button"
 export default function Main() {
     return(
         <div>
             <main>
                 <ContentPhoto></ContentPhoto>
                 <Heading></Heading>
+                <Button></Button>
             </main>
         </div>
     )
