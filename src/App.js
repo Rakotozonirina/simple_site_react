4e01719f2fd47d934c7f48b7504606d9de5ee8d6
Zo Nirina <tomzonirina@gmail.com>
2022-11-29 23:30:38 +0300

    yeah I finish the placement of hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../src/index.css'
 import Nav from './component/nav'
+import Hero from './component/hero'
 export default function App() {
     return(
         <>
             <Nav></Nav>
+            <Hero></Hero>
         </>
     )
 }
