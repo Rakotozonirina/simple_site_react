653aa63c5e923b0529dba2a1b7cef06bfe4e6bdc
Zo Nirina <tomzonirina@gmail.com>
2022-11-29 22:40:01 +0300

    Lets to begin the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import '../src/index.css'
+import Nav from './component/nav'
 export default function App() {
     return(
         <>
+            <Nav></Nav>
         </>
     )
 }
