0fa904d0718e097a810f574ab2637aee10d3906b
Zo Nirina <tomzonirina@gmail.com>
2022-11-29 22:10:58 +0300

    I remove the project I

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from 'react'
-import Nav from './component/navbar'
-import Heading from './component/heading'
-import Main from './component/main'
 export default function App() {
     return(
         <>
-            <Nav/>
-            <Heading/>
-            <Main/>
         </>
     )
 }
