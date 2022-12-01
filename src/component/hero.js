964db12c554704d73bf08d58388902bbbd5a8da5
Zo Nirina <tomzonirina@gmail.com>
2022-12-01 11:41:04 +0300

    I finish the heading Hero

--- src/component/hero.js
+++ src/component/hero.js
@@ -5,6 +5,10 @@ export default function Hero() {
     return(
         <section className='hero'>
             <img src={Image} alt='hero' loading="eager" decoding="sync"/>
+            <div>
+                <p>Online Experiences</p>
+                <p>Join unique interactive activities led by <br></br> one of a kinds hosts all without leaving <br></br> home.</p>
+            </div>
         </section>
     )
 }
