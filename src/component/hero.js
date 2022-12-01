054c56d13a90d0e65ef1c4cc0f21976345e72c8c
Zo Nirina <tomzonirina@gmail.com>
2022-12-01 10:40:23 +0300

    I finish the responsive of the nav and the hero content

--- src/component/hero.js
+++ src/component/hero.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Image from '../image/hero.png'
 
 export default function Hero() {
     return(
         <section className='hero'>
-
+            <img src={Image} alt='hero' loading="eager" decoding="sync"/>
         </section>
     )
 }
