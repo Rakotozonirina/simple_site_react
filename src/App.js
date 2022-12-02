ee3f6b95e4577e6058b45f412814f4549ececdb1
Zo Nirina <tomzonirina@gmail.com>
2022-12-02 11:07:33 +0300

    I finish the first card

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import '../src/index.css'
 import Nav from './component/nav'
 import Hero from './component/hero'
+import Card from './component/card'
 export default function App() {
     return(
         <>
             <Nav></Nav>
             <Hero></Hero>
+            <Card></Card>
         </>
     )
 }
