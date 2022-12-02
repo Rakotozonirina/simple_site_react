66c04d3903e6f23df2c754a2b8664759a53810b6
Zo Nirina <tomzonirina@gmail.com>
2022-12-02 16:32:46 +0300

    Yeah I am so happy cuz i termined the props of the image

--- src/App.js
+++ src/App.js
@@ -3,17 +3,28 @@ import '../src/index.css'
 import Nav from './component/nav'
 import Hero from './component/hero'
 import Card from './component/card'
+import images from './component/image'
 export default function App() {
     return(
         <>
             <Nav/>
             <Hero/>
             <div className='content--card'>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
+                <Card
+                    img={images.swimming}
+                />
+                <Card
+                    img={images.nature}
+                />
+                <Card
+                    img={images.tree}
+                />
+                <Card
+                    img={images.foggy}
+                />
+                <Card 
+                    img={images.landscape}
+                />
             </div>
         </>
     )
